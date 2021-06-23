2aa3cebbfe1fbc36d5647cec07064d018a0e7cbc
aditya <adityachoudhary54@gmail.com>
2021-06-23 14:30:17 +0530

    %1

---
+++ starterInitializerForGit.sh
@@ -0,0 +1,6 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/adityachoudary54/BackendFromLinux.git
+git push -u origin master
